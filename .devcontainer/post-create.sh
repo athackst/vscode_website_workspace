45e1de2342ceda3a2a0c8eccbec8e6d259002a60
Allison Thackston <allison@allisonthackston.com>
2022-12-22 18:10:42 +0000

    Support ruby 3.0

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+# Get desired ruby version
+export RUBY_VERSION=$(cat .ruby-version)
+
+# Install the desired ruby version
+rbenv install ${RUBY_VERSION}
+echo 'eval "$(rbenv init - bash)"' >> ~/.bashrc
+source ~/.bashrc
+
 # Install the version of Bundler.
 if [ -f Gemfile.lock ] && grep "BUNDLED WITH" Gemfile.lock > /dev/null; then
     cat Gemfile.lock | tail -n 2 | grep -C2 "BUNDLED WITH" | tail -n 1 | xargs gem install bundler -v
---
+++ index.md
@@ -0,0 +1,6 @@
+---
+---
+
+# Welcome to My Home Page
+
+This is a test page.
