fb72b99f9733d8e498e2bbdd1c39f8e94aa247c8
Allison Thackston <allison@allisonthackston.com>
2022-12-22 16:06:46 -0800

    Revert "Support ruby 3.0 (#18)"
    This reverts commit 654cfe0778fa43625dfe7490864c96bb6c4059b6.

--- .github/htmlproofer.rb
+++ .github/htmlproofer.rb
@@ -2,16 +2,14 @@
 require 'html-proofer'
 
 options = {
-  hydra: { max_concurrency: 50 },
+  parallel: { in_processes: 3 },
   typhoeus: {
     ssl_verifypeer: false,
     ssl_verifyhost: 0,
     timeout: 120,
-    connecttimeout: 30,
-    cookiefile: ".cookies",
-    cookiejar: ".cookies"
+    connecttimeout: 30
   },
-  ignore_urls: [/.*.md/]
+  ignore_urls: []
 }
 
 HTMLProofer.check_directory('./_site', options).run
